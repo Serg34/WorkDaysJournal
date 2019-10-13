c8d7d5d951efdbfc977e3939cfc5e08a4f6eba5b
Сергей <serg34_85@mail.ru>
2019-10-13 10:27:42 +0300

    Добавьте файлы проекта.

---
+++ Linq2db_MVP_IoC_DevExpress_WinForm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Furmanov;
+
+namespace Linq2db_MVP_IoC_DevExpress_WinForm
+{
+	static class Program
+	{
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainView());
+		}
+	}
+}
